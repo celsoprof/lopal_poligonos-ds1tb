975ef97560345295e19eee88284e67df3672bf72
CELSO MARCOS FURTADO <sn1006137@CFP127ALUNO.LOCAL>
2025-03-10 16:08:42 -0300

    Atividade 01 - Java

---
+++ src/br/senai/sp/jandira/calculo_poligonos/CalculadoraPoligonos.java
@@ -0,0 +1,15 @@
+package br.senai.sp.jandira.calculo_poligonos;
+
+import br.senai.sp.jandira.calculo_poligonos.model.Quadrado;
+
+public class CalculadoraPoligonos {
+
+	public static void main(String[] args) {
+		
+		Quadrado quadrado1 = new Quadrado();
+		quadrado1.setLado(16);
+		quadrado1.exibirDados();
+
+	}
+
+}
